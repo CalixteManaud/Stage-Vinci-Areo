2cdea16e2c98888506d31120c778f5331448d718
Manaud <manaud57@gmil.com>
2022-04-29 12:00:44 +0200

    Phase de test

--- test.py
+++ test.py
@@ -7,12 +7,43 @@ dev = usb.core.find(idVendor=0x04d8, idProduct=0x0070)
 
 if dev is None:
     raise ValueError('Device is not found')
+else:
+    print(dev)
+    dev.set_configuration()
 
-print(dev)
 
-dev.set_configuration()
+def send(cmd):
+    dev.write(3, cmd)
+    result = (dev.read(0x81, 100000, 1000))
+    return result
 
-msg = ':SDSLSCPI#'
-print("Write:", msg, dev.write(3, msg))
 
-print("Read:", dev.read(0x81, 7))
+def get_id():
+    return send('*IDN?').tobytes().decode('utf-8')
+
+
+def get_data(ch):
+    rawdata = send(':DATA:WAVE:SCREen:CH{}?'.format(ch))
+    data = []
+    for idx in range(4, len(rawdata), 2):
+        point = int().from_bytes([rawdata[idx], rawdata[idx + 1]], 'little', signed=True)
+        data.append(point / 4096)
+    return data
+
+
+def get_header():
+    header = send(':DATA:WAVE:SCREen:HEAD?')
+    header = header[4:].tobytes.decode('utf-8')
+    return header
+
+
+def save_data(ffname, data):
+    f = open(ffname, 'w')
+    f.write('\n'.join(map(str, data)))
+    f.close()
+
+
+print(get_id())
+header = get_header()
+data = get_data(1)
+save_data('Osci.dat', data)
